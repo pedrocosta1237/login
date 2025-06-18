c887d4f9d21b241e268f23b1e7fa391892cb4403
pedrocosta1237 <pedro.costa195@etec.sp.gov.br>
2025-06-18 12:31:49 -0300

    Create login.php

---
+++ public/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form action="process_login.php" method="POST">
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <label>Senha:</label>
+        <input type="password" name="senha" required>
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
